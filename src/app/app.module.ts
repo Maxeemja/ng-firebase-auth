a7abd08fcf2e7ecbe12f095b819bdd85aa1a327a
Maksym Hrytsiuk <maksym_hrytsiuk@epam.com>
2022-11-11 14:37:36 +0200

    bootstrap added, login component created, initialized auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-JQck5pPPM_Jnch2lnt-dg19rG-JbFHE",
+    authDomain: "ng-firebase-auth-86f36.firebaseapp.com",
+    projectId: "ng-firebase-auth-86f36",
+    storageBucket: "ng-firebase-auth-86f36.appspot.com",
+    messagingSenderId: "507189884388",
+    appId: "1:507189884388:web:92d4d36a923f86778c83f1",
+    measurementId: "G-CV7XLCLF6N"
+  }
 };
 
 /*
