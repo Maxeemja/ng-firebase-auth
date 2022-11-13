bc19bdd5312e2628b5f25bfa8f80a3c4c0a005f8
Maksym Hrytsiuk <maksym_hrytsiuk@epam.com>
2022-11-13 23:03:44 +0200

    configs fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-JQck5pPPM_Jnch2lnt-dg19rG-JbFHE",
+    authDomain: "ng-firebase-auth-86f36.firebaseapp.com",
+    projectId: "ng-firebase-auth-86f36",
+    storageBucket: "ng-firebase-auth-86f36.appspot.com",
+    messagingSenderId: "507189884388",
+    appId: "1:507189884388:web:92d4d36a923f86778c83f1",
+    measurementId: "G-CV7XLCLF6N"
+  }
 };
